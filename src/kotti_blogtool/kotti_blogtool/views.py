76b09a95e14d3a5ccf87f28a9ccc1c3ed0520df6
Christoph Boehner <cb@vorwaerts-werbung.de>
2013-11-05 00:06:45 +0100

    Copy blogging tool content as example

--- src/kotti_blogtool/kotti_blogtool/views.py
+++ src/kotti_blogtool/kotti_blogtool/views.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
+import datetime
+from dateutil.tz import tzutc
 
+from colander import deferred
 from colander import SchemaNode
 from colander import String
+from colander import Range
+from colander import DateTime
+from colander import iso8601
+from deform.widget import DateTimeInputWidget
 from kotti.views.edit.content import ContentSchema
+from kotti.views.edit import DocumentSchema
 from kotti.views.form import AddFormView
 from kotti.views.form import EditFormView
 from pyramid.view import view_config
@@ -13,14 +21,52 @@ from kotti_blogtool.resources import Blog
 from kotti_blogtool.fanstatic import kotti_blogtool
 
 
-class BlogSchema(ContentSchema):
-    """Schema for add / edit forms of Blog"""
+@deferred
+def deferred_date_missing(node, kw):
+    value = datetime.datetime.now()
+    return datetime.datetime(
+        value.year, value.month, value.day, value.hour,
+        value.minute, value.second, value.microsecond, tzinfo=tzutc())
 
-    example_attribute = SchemaNode(
-        String(),
-        title=_(u'Example Attribute'),
-        missing=u"",
+
+class BlogSchema(DocumentSchema):
+    """ Blog schema derived from basic documents """
+    pass
+
+
+class BlogEntrySchema(DocumentSchema):
+    date = SchemaNode(
+        DateTime(),
+        title=_(u'Date'),
+        description=_(u'Choose date of the blog entry. '
+                      u'If you leave this empty the creation date is used.'),
+        validator=Range(
+            min=datetime.datetime(
+                2012, 1, 1, 0, 0, tzinfo=iso8601.Utc()),
+            min_err=_('${val} is earlier than earliest datetime ${min}')),
+        widget=DateTimeInputWidget(),
+        missing=deferred_date_missing,
     )
+
+
+class BlogAddForm(AddFormView):
+    schema_factory = BlogSchema
+    add = Blog
+    item_type = _(u"Blog")
+
+
+class BlogEditForm(EditFormView):
+    schema_factory = BlogSchema
+
+
+class BlogEntryAddForm(AddFormView):
+    schema_factory = BlogEntrySchema
+    add = BlogEntry
+    item_type = _(u"Blog Entry")
+
+
+class BlogEntryEditForm(EditFormView):
+    schema_factory = BlogEntrySchema
 
 
 @view_config(name=Blog.type_info.add_view,
